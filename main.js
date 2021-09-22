5566bfcdf5c157e62e19ded7acf084da8469a108
brunoJorquera <jorquera.b18@gmail.com>
2021-09-21 21:38:51 -0700

    Assessment redo, CRUD

---
+++ main.js
@@ -0,0 +1,67 @@
+const fortuneBtn = document.getElementById('fortune-btn')
+const fortuneContainer = document.getElementById('fortune-display')
+const addFortuneForm = document.getElementById('add-fortune-form')
+const phraseBtn = document.getElementById('phrase-btn')
+const phraseContainer = document.getElementById('phrase-display')
+const addPhraseForm = document.getElementById('phrase-form')
+
+document.getElementById("complimentButton").onclick = function () {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+  };
+
+fortuneBtn.addEventListener('click', () => {
+    axios.get('http://localhost:4000/api/fortune')
+        .then(res => {
+            fortuneContainer.innerHTML = ''
+            const fortuneElement = document.createElement('li')
+            console.log(fortuneElement)
+            fortuneElement.textContent = res.data
+            fortuneContainer.append(fortuneElement)
+            console.log(res.data)
+        })
+        .catch(err => console.log(err))
+})
+
+addFortuneForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const addFortuneInput = document.getElementById('fortune-input')
+    const {value} = addFortuneInput
+
+    axios.post('http://localhost:4000/api/fortune', {value})
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(err => console.log(err))
+
+})
+
+phraseBtn.addEventListener('click', () => {
+    axios.get('http://localhost:4000/api/phrase')
+        .then(res => {
+            phraseContainer.innerHTML = 'Available Phrases:'
+            const phraseElement = document.createElement('p')
+            phraseElement.textContent = res.data
+            phraseContainer.append(phraseElement)
+            console.log(res.data)
+        })
+        .catch(err => console.log(err))
+})
+
+addPhraseForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const addPhraseInput = document.getElementById('phrase-input')
+    const {value} = addPhraseInput
+
+    axios.post('http://localhost:4000/api/phrase', {value})
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(err => console.log(err))
+
+})
---
+++ server/controllers/controller.js
@@ -0,0 +1,40 @@
+const fortunes = [
+    'Look both ways before crossing the street',
+    'A fortune sought is a fortune earned',
+    'Taking the time to fix a mistake can prove to be fruitful in the longrun', 
+    'You will find love in the most unconventional of places',
+    'The five second rule is a lie'
+]
+const phrases = [
+    ' Duty ',
+    ' Ubiquitous ',
+    ' Coccyx ',
+    ' Moist '
+]
+
+module.exports = {
+    getFortune: (req,res) => {
+        const randIndex = Math.floor(Math.random() * fortunes.length)
+        let fortune = fortunes[randIndex]
+        res.status(200).send(fortune)
+    },
+    addFortune: (req,res) => {
+        // console.log('I work')
+        // console.log(req.body)
+        const {value} = req.body
+        fortunes.push(value)
+
+        res.status(200).send(fortunes)
+    },
+    getPhrase: (req,res) => {
+        res.status(200).send(phrases)
+    },
+    addPhrase: (req,res) =>{
+        // console.log('I work too!')
+        // console.log(res.body)
+        const {value} = req.body
+        phrases.push(value)
+
+        res.status(200).send(phrases)
+    }
+}
